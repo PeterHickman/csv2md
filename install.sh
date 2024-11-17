7733ef42cee625daad597194c1532e587043b454
Peter Hickman <peterhickman386@googlemail.com>
2024-11-17 21:02:38 +0000

    clean up the build

--- install.sh
+++ install.sh
@@ -1,24 +1,13 @@
 #!/bin/sh
 
-if ! [ "$(id -u)" = 0 ]; then
-  echo 'You must be root to do this.' 1>&2
-  exit 1
-fi
+BINARY='/usr/local/bin'
+APP=csv2md
 
-##
-# When installing we would install as root, however on
-# BSD based systems (such as OSX) this is not a valid
-# group / user for install so we use the numeric value
-##
-ROOT="0"
+echo "Building $APP"
+go build -ldflags="-s -w" $APP.go
 
-TARGET="/usr/local/bin/csv2md"
+echo "Installing $APP to $BINARY"
+install $APP $BINARY
 
-echo "Building csv2md"
-go build csv2md.go
-
-echo "Installing csv2md to $TARGET"
-install -g $ROOT -o $ROOT -m 0755 csv2md $TARGET
-
-echo "Removing the build artefact"
-rm csv2md
+echo "Removing the build"
+rm $APP
